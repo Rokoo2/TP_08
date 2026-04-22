902510f825aba3c886265ce8dc5cb0b7180183d7
Rokoo2 <nicoonunez12@gmail.com>
2026-04-22 09:12:10 -0300

    Initial commit

---
+++ Assets/Scripts/PasswordUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PasswordUI : MonoBehaviour
+{
+  
+  public TextMeshProUGUI Txt_Resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckPassword();
+    }
+
+    public void CheckPassword()
+    {
+        Txt_Resultado.text = "Acá va el resultado";
+
+    }
+
+
+}
